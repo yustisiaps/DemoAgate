5f4e37912118c17516e11c46018dc5ea1051aa4b
Yustisia Pratiwi Suhastri <32694093+yustisiaps@users.noreply.github.com>
2025-11-10 13:53:00 +0700

    nambah script

--- Assets/Script/contoh selanjutnya.cs
+++ Assets/Script/contoh selanjutnya.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class contoh2 : MonoBehaviour
+public class contohselanjutnya : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
