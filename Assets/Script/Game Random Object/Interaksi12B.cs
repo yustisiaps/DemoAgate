945eab6c3a661f5b830fe9f9b3f1fc94177d1132
Yustisia Pratiwi Suhastri <32694093+yustisiaps@users.noreply.github.com>
2025-11-13 09:21:06 +0700

    Comment code
    11/13/2025

--- Assets/Script/Game Random Object/Interaksi12B.cs
+++ Assets/Script/Game Random Object/Interaksi12B.cs
@@ -1,8 +1,12 @@
+using TMPro;
 using UnityEngine;
 
 public class Interaksi12B : MonoBehaviour
 {
+    //dekalarasi variabel
     public SquareMove12 squareMove12;
+    public int skor;
+    
     void Start()
     {
         
@@ -13,20 +17,37 @@ public class Interaksi12B : MonoBehaviour
     {
 
     }
+
+    //pendeteksi input dari tombol arah
+    //dipakai untuk menggerakkan objek
+
+    // Fungsi ini akan dipanggil ketika tombol "Kiri" ditekan.
+    // Tugasnya adalah mengubah arah gerak objek menjadi ke kiri.
+
     public void GerakKiri()
     {
-        squareMove12.arahGerakB = ArahGerakB.Kiri;
+        // Mengatur arah gerak pada script SquareMove12 menjadi Kiri.
+        squareMove12.arah = ArahGerakB.Kiri;
     }
+    // Fungsi ini dipanggil ketika tombol "Kanan" ditekan.
+    // Mengubah arah gerak objek ke arah kanan.
     public void GerakKanan()
     {
+        // Mengatur arah gerak objek ke Kanan.
         squareMove12.arahGerakB = ArahGerakB.Kanan;
     }
+    // Fungsi ini dipanggil ketika tombol "Atas" ditekan.
+    // Mengubah arah gerak objek ke atas.
     public void GerakAtas()
     {
+        // Mengatur arah gerak objek ke Atas.
         squareMove12.arahGerakB = ArahGerakB.Atas;
     }
+    // Fungsi ini dipanggil ketika tombol "Bawah" ditekan.
+    // Mengubah arah gerak objek ke bawah.
     public void GerakBawah()
     {
+        // Mengatur arah gerak objek ke Bawah.
         squareMove12.arahGerakB = ArahGerakB.Bawah;
     }
 }
--- Assets/Script/Game Random Object/Square Move Move.cs
+++ Assets/Script/Game Random Object/Square Move Move.cs
@@ -1,10 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class SquareMoveMove : MonoBehaviour
 {
+    //Deklarasi variabel arahGerakB dengan tipe data ArahGerakB
     public ArahGerakB arahGerakB = ArahGerakB.Kanan;
-
+    //Deklarasi variabel interaksi12B dengan tipe data Interaksi12B
     public Interaksi12B interaksi12B;
+    //deklarasi variabel speed dengan tipe data float untuk mengatur kecepatan gerak objek
     public float speed = 5.0f;
 
     void Start()
@@ -15,8 +18,10 @@ public class SquareMoveMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Mengecek arah gerak dan menggerakkan objek sesuai arah yang ditentukan
         switch (arahGerakB)
         {
+            // Menentukan gerakan berdasarkan arahGerakB yang dipilih kiri, kanan, atas, atau bawah
             case ArahGerakB.Kiri:
                 transform.Translate(-speed * Time.deltaTime, 0, 0);
                 break;
@@ -32,12 +37,15 @@ public class SquareMoveMove : MonoBehaviour
         }
 
     }
-
+    //untuk mendeteksi tabrakan dengan objek lain
     void OnCollisionEnter2D(Collision2D collision)
     {
+        //untuk mendeteksi tabrakan dengan objek yang memiliki tag "Dinding"
         if (collision.gameObject.CompareTag("Dinding"))
         {
+            //untuk membalik arah gerak objek saat bertabrakan dengan dinding
             Debug.Log("Bertabrakan dengan dinding");
+            //untuk membalik arah gerak objek
             if (arahGerakB == ArahGerakB.Kanan)
             {
                 arahGerakB = ArahGerakB.Kiri;
@@ -55,11 +63,12 @@ public class SquareMoveMove : MonoBehaviour
                 arahGerakB = ArahGerakB.Atas;
             }
         }
-
+    } 
+    public void TambahSkor()
+    {
+        interaksi12B.Score.text = "Skor:" + interaksi12B.skor.ToString();
+        //mengubah Score  di script Interaksi12 menjadi "Skor:" diikuti dengan nilai skor
     }
-    
-    public void TambahSkor
-    
 }
 public enum ArahGerakB
     {
